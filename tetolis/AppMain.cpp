9c5722d12870049e24f73fa1ab99ffbca2a9bfe9
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-10-03 15:04:05 +0900

    ヘッダーファイルの作成
    ヘッダーファイルをつくった

--- tetolis/AppMain.cpp
+++ tetolis/AppMain.cpp
@@ -1,4 +1,18 @@
+/***************************************
+* プログラミング実習（テトリス編）
+***************************************/
 #include "DxLib.h"
+#include "FreamControl.h"
+#include "SceneManager.h"
+#include "InputControl.h"
+
+/***************************************
+* マクロ定義）
+***************************************/
+#define SCREEN_HEIGHT (720)
+#define SCREEN_WIDTH (1280)
+#define SCREEN_COLORBIT  (32)
+#define FONT_SIZE (20)
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR IpCmdLine, _In_ int nShowCmd)
 {
