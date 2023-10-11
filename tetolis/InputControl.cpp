f784e2bc83151eda92592e4e927ad3baf85cbd16
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-10-11 15:02:12 +0900

    cpp書き込み
    cppに書き込んだ

--- tetolis/InputControl.cpp
+++ tetolis/InputControl.cpp
@@ -19,6 +19,11 @@ enum KEY_STATE
 };
 
 /***************************************
+* 変数宣言
+***************************************/
+KEY_STATE button_state[XINPUT_BUTTON_MAX];  //各ボタン入力状態
+
+/***************************************
 * プロトタイプ宣言
 ***************************************/
 
@@ -58,10 +63,146 @@ void InputControl_Update(void)
 		{
 			switch (button_state[i])
 			{
-
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_CLICK;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				button_state[i] = E_PRESS;
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
+			}
+		}
+		else
+		{
+			switch (button_state[i])
+			{
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_NONE;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				button_state[i] = E_RELEASED;
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
 			}
 		}
 	}
 
+}
 
+/***************************************
+* 入力制御機能:押されているか判定処理
+* 引数:XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD_UP  (0)  //デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD_DOWN  (1)  //デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD_LEFT  (2)  //デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD_RIGHT  (3)  //デジタル方向ボタン右
+* XINPUT_BUTTON_START  (4)  //STARTボタン
+* XINPUT_BUTTON_BACK  (5)  //BACKボタン
+* XINPUT_BUTTON_LEFT_THUMB  (6)  //左スティック押し込み
+* XINPUT_BUTTON_RIGHT_THUMB  (7)  //右スティック押し込み
+* XINPUT_BUTTON_LEFT_SHOULDER  (8)  //LBボタン
+* XINPUT_BUTTON_RIGHT_SYOULDER  (9)  //RBボタン
+* XINPUT_BUTTON_A  (12)  //Aボタン
+* XINPUT_BUTTON_B  (13)  //Bボタン
+* XINPUT_BUTTON_X  (14)  //Xボタン
+* XINPUT_BUTTON_Y  (15)  //Yボタン
+* 戻り値:TRUE（押されている）,FALSE（押されていない）
+***************************************/
+int GetButton(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_CLICK || button_state[button] == E_PRESS)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+/***************************************
+* 入力制御機能:押した瞬間か判定処理
+* 引数:XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD_UP  (0)  //デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD_DOWN  (1)  //デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD_LEFT  (2)  //デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD_RIGHT  (3)  //デジタル方向ボタン右
+* XINPUT_BUTTON_START  (4)  //STARTボタン
+* XINPUT_BUTTON_BACK  (5)  //BACKボタン
+* XINPUT_BUTTON_LEFT_THUMB  (6)  //左スティック押し込み
+* XINPUT_BUTTON_RIGHT_THUMB  (7)  //右スティック押し込み
+* XINPUT_BUTTON_LEFT_SHOULDER  (8)  //LBボタン
+* XINPUT_BUTTON_RIGHT_SYOULDER  (9)  //RBボタン
+* XINPUT_BUTTON_A  (12)  //Aボタン
+* XINPUT_BUTTON_B  (13)  //Bボタン
+* XINPUT_BUTTON_X  (14)  //Xボタン
+* XINPUT_BUTTON_Y  (15)  //Yボタン
+* 戻り値:TRUE（押した瞬間）,FALSE（押した瞬間ではない）
+***************************************/
+int GetButtonDown(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_CLICK)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+/***************************************
+* 入力制御機能:離したか判定処理
+* 引数:XINPUTのボタン情報
+* XINPUT_BUTTON_DPAD_UP  (0)  //デジタル方向ボタン上
+* XINPUT_BUTTON_DPAD_DOWN  (1)  //デジタル方向ボタン下
+* XINPUT_BUTTON_DPAD_LEFT  (2)  //デジタル方向ボタン左
+* XINPUT_BUTTON_DPAD_RIGHT  (3)  //デジタル方向ボタン右
+* XINPUT_BUTTON_START  (4)  //STARTボタン
+* XINPUT_BUTTON_BACK  (5)  //BACKボタン
+* XINPUT_BUTTON_LEFT_THUMB  (6)  //左スティック押し込み
+* XINPUT_BUTTON_RIGHT_THUMB  (7)  //右スティック押し込み
+* XINPUT_BUTTON_LEFT_SHOULDER  (8)  //LBボタン
+* XINPUT_BUTTON_RIGHT_SYOULDER  (9)  //RBボタン
+* XINPUT_BUTTON_A  (12)  //Aボタン
+* XINPUT_BUTTON_B  (13)  //Bボタン
+* XINPUT_BUTTON_X  (14)  //Xボタン
+* XINPUT_BUTTON_Y  (15)  //Yボタン
+* 戻り値:TRUE（押した瞬間）,FALSE（押した瞬間ではない）
+***************************************/
+int GetButtonUp(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_RELEASED)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+/***************************************
+* 入力制御機能:ゲーム終了用のボタン判定処理
+* 引数:なし
+* 戻り値:TRUE（押した瞬間）,FALSE（押した瞬間ではない）
+***************************************/
+int GetExitButton(void)
+{
+	int ret = FALSE;
+
+	if ((GetButtonDown(XINPUT_BUTTON_BACK) == TRUE) || (CheckHitKey(KEY_INPUT_ESCAPE) == TRUE))
+	{
+		ret = TRUE;
+	}
+
+	return ret;
 }
---
+++ tetolis/RankingScene.cpp
@@ -0,0 +1,289 @@
+#include "RankingScene.h"
+#include "DxLib.h"
+#include "InputControl.h"
+#include "SceneManager.h"
+
+/***************************************
+* マクロ定義
+***************************************/
+#define RANKING_FILE  ("dat/rankingdate.csv")
+#define RANKING_MAX  (10)
+#define RANKING_NAME_LEN  (11)
+
+/***************************************
+* 型定義
+***************************************/
+typedef struct
+{
+	int rank;
+	char name[RANKING_NAME_LEN];  //名前
+	int score;
+}T_RANKING;
+
+typedef struct
+{
+	int x;
+	int y;
+	
+}T_CURSOR;
+
+/***************************************
+* グローバル変数宣言
+***************************************/
+T_RANKING Ranking_Date[RANKING_MAX];  //ランキングデータ
+T_RANKING New_Score;  //新しいスコアデータ
+int DispMode;
+
+T_CURSOR Cursor;
+int name_num;
+/***************************************
+* プロトタイプ宣言
+***************************************/
+void file_read(void);  //ファイル読み込み
+void file_write(void);  //ファイル書き込み
+void ranking_sort(void);  //ランキングソート処理
+void ranking_input_name(void);  //名前入力処理
+void ranking_input_name_draw(void);  //名前入力描画処理
+
+/***************************************
+* ランキング画面：初期化処理
+* 引数：なし
+* 戻り値：エラー情報（-1:異常有、-1以外：正常終了）
+***************************************/
+int RankingScene_Initialize(void)
+{
+	int ret = 0;
+
+	file_read();
+
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		Cursor.x = 0;
+		Cursor.y = 0;
+		name_num = 0;
+		break;
+	case RANKING_DISP_MODE:
+	default:
+
+		break;
+	}
+
+	return ret;
+}
+
+/***************************************
+* ランキング画面：更新処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void RankingScene_Update(void)
+{
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		ranking_input_name();
+		break;
+	case RANKING_DISP_MODE:
+	default:
+		if (GetButtonDown(XINPUT_BUTTON_B))
+		{
+			Change_Scene(E_TITLE);
+		}
+		break;
+	}
+}
+
+/***************************************
+* ランキング画面：描画処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void RankingScene_Draw(void)
+{
+	int i;
+
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		ranking_input_name_draw();
+		break;
+	case RANKING_DISP_MODE:
+	default:
+		for (i = 0; i < RANKING_MAX; i++)
+		{
+			DrawFormatString(20, 10 + (i * 25), GetColor(255, 255, 255), "%2d,%10s,%10d", Ranking_Date[i].name, Ranking_Date[i].score);
+		}
+		break;
+	}
+}
+
+/***************************************
+* ランキング画面：画面変更処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void Set_RankingMode(int mode)
+{
+	DispMode = mode;
+}
+
+/***************************************
+* ランキング画面：スコア取得処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void Set_RankingScore(int score)
+{
+	New_Score.score = score;
+}
+
+/***************************************
+* ランキング画面：ファイル読み込み処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void file_read(void)
+{
+	FILE* fp = NULL;
+	int i;
+
+	OutputDebugString("ファイルが読み込みます");
+	fopen_s(&fp, RANKING_FILE, "r");
+
+	if (fp == NULL)
+	{
+		OutputDebugString("ファイルが読み込めません");
+		OutputDebugString("ファイルを生成します");
+		file_write();
+	}
+	else
+	{
+		for (i = 0; i < RANKING_MAX; i++)
+		{
+			fscanf_s(fp, "%2d,%[^,],%10d\n", &Ranking_Date[i].rank, Ranking_Date[i].name, RANKING_NAME_LEN, &Ranking_Date[i].score);
+		}
+
+		fclose(fp);
+	}
+}
+
+/***************************************
+* ランキング画面：ファイル書み込み処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void file_write(void)
+{
+	FILE* fp = NULL;
+	int i;
+
+	OutputDebugString("ファイルが書き込みます");
+	fopen_s(&fp, RANKING_FILE, "w");
+
+	if (fp == NULL)
+	{
+		OutputDebugString("ファイルが書き込めません");
+		
+	}
+	else
+	{
+		for (i = 0; i < RANKING_MAX; i++)
+		{
+			fscanf_s(fp, "%2d,%[^,],%10d\n", &Ranking_Date[i].rank, Ranking_Date[i].name, RANKING_NAME_LEN, &Ranking_Date[i].score);
+		}
+
+		fclose(fp);
+	}
+}
+
+/***************************************
+* ランキング画面：ランキングソート処理処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void ranking_sort(void)
+{
+	int i, j;  //ループカウンタ
+	T_RANKING tmp;  //退避領域
+
+	//一番下のスコアを更新する
+	Ranking_Date[RANKING_MAX - 1] = New_Score;
+
+	//データのソートを行う
+	for (i = 0; i < RANKING_MAX; i++)
+	{
+		for (j = i + 1; j < RANKING_MAX; j++)
+		{
+			if (Ranking_Date[i].score < Ranking_Date[j].score)
+			{
+				tmp = Ranking_Date[i];
+				Ranking_Date[i]= Ranking_Date[j];
+				Ranking_Date[j] = tmp;
+			}
+		}
+	}
+
+	//順位を上からふっていく
+	for (i = 0; i < RANKING_MAX; i++)
+	{
+		Ranking_Date[i].rank = i + 1;
+	}
+
+	//ファイルに書き込みを行う
+	file_write();
+}
+
+/***************************************
+* ランキング画面：名前入力処理処理
+* 引数：なし
+* 戻り値：なし
+***************************************/
+void ranking_input_name(void)
+{
+	int c;
+
+	//カーソル操作処理
+	if (GetButtonDown(XINPUT_BUTTON_DPAD_LEFT) == TRUE)
+	{
+		if (Cursor.x < 0)
+		{
+			Cursor.x--;
+		}
+	}
+	if (GetButtonDown(XINPUT_BUTTON_DPAD_RIGHT) == TRUE)
+	{
+		if (Cursor.x < 12)
+		{
+			Cursor.x++;
+		}
+	}
+	if (GetButtonDown(XINPUT_BUTTON_DPAD_UP) == TRUE)
+	{
+		if (Cursor.y > 0)
+		{
+			Cursor.y--;
+		}
+	}
+	if (GetButtonDown(XINPUT_BUTTON_DPAD_DOWN) == TRUE)
+	{
+		if (Cursor.y < 4)
+		{
+			Cursor.y++;
+		}
+	}
+
+	//文字を選択する
+	if (GetButtonDown(XINPUT_BUTTON_B) == TRUE)
+	{
+		if (Cursor.y < 2)
+		{
+			c = 'a' + Cursor.x + (Cursor.y * 13);
+			New_Score.name[name_num++] = c;
+		}
+		else if (Cursor.y < 4)
+		{
+
+		}
+	}
+}
