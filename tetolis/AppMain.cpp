5d56f4536d289ec8f6fa3c2fbec31e355dbf69d3
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-11-01 15:02:42 +0900

    ファイル追加
    ファイルをダウンロードした

--- tetolis/AppMain.cpp
+++ tetolis/AppMain.cpp
@@ -11,7 +11,7 @@
 ***************************************/
 #define SCREEN_HEIGHT (720)
 #define SCREEN_WIDTH (1280)
-#define SCREEN_COLORBIT  (32)
+#define SCREEN_COLORBIT   (32)
 #define FONT_SIZE (20)
 
 /***************************************
@@ -63,7 +63,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SetFontSize(FONT_SIZE);
 
 	//ゲームループ
-	while (ProcessMessage() == 0 && GetExitButton() != TRUE && ErrorCheck()==D_NORMALITY)
+	while (ProcessMessage() == 0 && GetExitButton() != TRUE && ErrorCheck() == D_NORMALITY)
 	{
 		//フレーム制御機能更新処理
 		FreamControl_Update();
